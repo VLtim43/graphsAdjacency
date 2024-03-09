2dac4818a95a9e37ae68b3cebde5d5324e54db76
VLtim43 <fernandoreal.valentim@gmail.com>
2024-03-09 19:02:17 -0300

    initial interface

---
+++ interface.js
@@ -0,0 +1,48 @@
+// interface.js
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("Graph Console Interface");
+
+function askQuestion(query) {
+  return new Promise((resolve) => {
+    rl.question(query, (answer) => {
+      resolve(answer);
+    });
+  });
+}
+
+async function getNumberOfNodes() {
+  let num;
+  do {
+    num = await askQuestion("Enter the number of nodes for the graph (1-10): ");
+    num = parseInt(num, 10);
+  } while (isNaN(num) || num < 1 || num > 10);
+  return num;
+}
+
+async function getGraphType() {
+  let type;
+  do {
+    type = await askQuestion(
+      "Enter the type of the graph (D for Directed/U for Undirected): "
+    );
+    type = type.toUpperCase();
+  } while (!["D", "U"].includes(type));
+  return type === "D" ? "Directed" : "Undirected";
+}
+
+async function main() {
+  const numberOfNodes = await getNumberOfNodes();
+  const graphType = await getGraphType();
+
+  console.log(`Creating a ${graphType} graph with ${numberOfNodes} nodes...`);
+
+  rl.close();
+}
+
+main().catch(console.error);
