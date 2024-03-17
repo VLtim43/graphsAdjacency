eaeae5178ace021cafb336c9dee5992ce65a5267
VLtim43 <fernandoreal.valentim@gmail.com>
2024-03-16 21:49:52 -0300

    interface

--- graph.mjs
+++ graph.mjs
@@ -4,12 +4,16 @@ export class Graph {
     this.matrix = this.#initiateMatrix(size);
   }
 
+  getSize() {
+    return this.size;
+  }
+
   #initiateMatrix(size) {
     let matrix = [];
     for (let i = 0; i < size; i++) {
-      matrix[i] = [];
+      matrix[i + 1] = [];
       for (let j = 0; j < size; j++) {
-        matrix[i][j] = 0;
+        matrix[i + 1][j + 1] = 0;
       }
     }
     return matrix;
@@ -21,6 +25,14 @@ export class Graph {
 
   removeNode(nodeA, nodeB) {
     this.matrix[nodeA][nodeB] = 0;
+  }
+
+  shuffle() {
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        this.matrix[i][j] = Math.round(Math.random());
+      }
+    }
   }
 
   displayMatrixAsTable() {
--- interface.mjs
+++ interface.mjs
@@ -1,13 +1,14 @@
 import { createInterface } from "readline";
-import { Graph } from "./grap.mjs";
+import { Graph } from "./graph.mjs";
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-console.log("Graph Console Interface");
-
+console.log("┌───────────────────────────┐");
+console.log("│  Graph Console Interface  │");
+console.log("└───────────────────────────┘");
 function askQuestion(query) {
   return new Promise((resolve) => {
     rl.question(query, (answer) => {
@@ -21,32 +22,72 @@ async function getNumberOfNodes() {
   let num;
   do {
     num = await askQuestion(
-      `Enter the number of nodes for the graph (1-${maxValue}): `
+      `➜ Enter the number of nodes for the graph (1-${maxValue}): `
     );
     num = parseInt(num, 10);
   } while (isNaN(num) || num < 1 || num > maxValue);
   return num;
 }
 
-// async function getGraphType() {
-//   let type;
-//   do {
-//     type = await askQuestion(
-//       "Enter the type of the graph (D for Directed/U for Undirected): "
-//     );
-//     type = type.toUpperCase();
-//   } while (!["D", "U"].includes(type));
-//   return type === "D" ? "Directed" : "Undirected";
-// }
+async function manageGraphOptions(grafo) {
+  let exit = false;
+  do {
+    console.log("➜ Choose an option ↴");
+    console.log("[1] Create a link between two nodes");
+    console.log("[2] Delete a link between two nodes");
+    console.log("[3] Shuffle the matrix(random nodes)");
+    console.log("[4] Display the matrix");
+    console.log("[5] Exit");
+
+    const option = await askQuestion("|");
+
+    switch (option) {
+      case "1":
+        let nodeA;
+        do {
+          nodeA = await askQuestion(`[Enter the first Node]:`);
+        } while (isNaN(nodeA) || nodeA < 1 || nodeA > grafo.getSize());
+
+        let nodeB;
+        do {
+          nodeB = await askQuestion(`[Enter the second Node]:`);
+        } while (isNaN(nodeB) || nodeB < 1 || nodeB > grafo.getSize());
+
+        grafo.linkNodes(parseInt(nodeA, 10), parseInt(nodeB, 10));
+        break;
+      case "2":
+        const nodeAForDeletion = await askQuestion(
+          "Enter the source node for deletion: "
+        );
+        const nodeBForDeletion = await askQuestion(
+          "Enter the target node for deletion: "
+        );
+        grafo.removeNode(
+          parseInt(nodeAForDeletion, 10),
+          parseInt(nodeBForDeletion, 10)
+        );
+        console.log("Link deleted.");
+        break;
+      case "3":
+        grafo.shuffle();
+        console.log("Matrix shuffled.");
+        break;
+      case "4":
+        grafo.displayMatrixAsTable();
+        break;
+      case "5":
+        exit = true;
+        break;
+      default:
+        console.log("Invalid option, please choose again.");
+    }
+  } while (!exit);
+}
 
 async function main() {
   const numberOfNodes = await getNumberOfNodes();
-  // const graphType = await getGraphType();
-
   const grafo = new Graph(numberOfNodes);
-
-  grafo.displayMatrixAsTable();
-
+  await manageGraphOptions(grafo);
   rl.close();
 }
 
